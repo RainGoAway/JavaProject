185192db1450e90a8b6df0216edaa735900b617d
RainGoAway <xzzxuzhen@163.com>
2018-05-08 22:46:42 +0800

    测试

---
+++ test/src/learnGitHub/HelloGitHub.java
@@ -0,0 +1,13 @@
+package learnGitHub;
+
+public class HelloGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hell,GitHub!");
+	}
+
+}
